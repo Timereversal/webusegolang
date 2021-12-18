bc4fd8729077fbb99f47a86d3aaab25e1e5c7a2a
berzoc <berzoc@gmail.com>
2021-12-18 17:26:19 -0600

    FAQ using template

--- main.go
+++ main.go
@@ -43,28 +43,10 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("data-1", "xx")
-	fmt.Fprint(w, `
-	<h1>FAQ Page</h1>
-<ul>
-  <li>
-    <b>Is there a free version?</b>
-    Yes! We offer a free trial for 30 days on any paid plans.
-  </li>
-  <li>
-    <b>What are your support hours?</b>
-    We have support staff answering emails 24/7, though response
-    times may be a bit slower on weekends.
-  </li>
-  <li>
-    <b>How do I contact support?</b>
-    Email us -
-    <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-  </li>
-</ul>
-	`)
-}
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 
+}
 func getUser(w http.ResponseWriter, r *http.Request) {
 	user := chi.URLParam(r, "userID")
 	fmt.Fprint(w, "inside User using Chi ", user)
@@ -121,6 +103,7 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Get("/home", homeHandler)
 	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
 	r.Route("/user", func(r chi.Router) {
 		r.Route("/{userID}", func(r chi.Router) {
 			r.Get("/", getUser)
