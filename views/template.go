c2a1d1195c7288ef64571feee4386e86d9530132
timereversal <berzoc@gmail.com>
2022-10-12 09:44:27 -0500

    csrf via data en template

--- views/template.go
+++ views/template.go
@@ -19,25 +19,35 @@ func Must(t Template, err error) Template {
 	return t
 }
 
-func ParseFS(fs fs.FS, pattern ...string) (Template, error) {
-	tpl, err := template.ParseFS(fs, pattern...)
+func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
+	tpl := template.New(patterns[0])
+	tpl = tpl.Funcs(
+		template.FuncMap{
+			"csrfField": func() template.HTML {
+				return `<input type="hidden" />`
+			},
+		},
+	)
+	tpl, err := tpl.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
 	}
+
 	return Template{
 		HTMLTpl: tpl,
 	}, nil
 }
 
-func Parse(filepath string) (Template, error) {
-	tpl, err := template.ParseFiles(filepath)
-	if err != nil {
-		return Template{}, fmt.Errorf("parsing template: %w", err)
-	}
-	return Template{
-		HTMLTpl: tpl,
-	}, nil
-}
+//
+//func Parse(filepath string) (Template, error) {
+//	tpl, err := template.ParseFiles(filepath)
+//	if err != nil {
+//		return Template{}, fmt.Errorf("parsing template: %w", err)
+//	}
+//	return Template{
+//		HTMLTpl: tpl,
+//	}, nil
+//}
 
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
